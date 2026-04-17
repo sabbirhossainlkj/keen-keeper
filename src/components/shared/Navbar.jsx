f24c0c51c76d45b93ac0a9101f8af6390151e169
sabbir <sabbirhossain1177034@gmail.com>
2026-04-17 17:30:34 +0600

    improve navbar with mobile dropdown menu and active link styling

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,32 +1,104 @@
-import React from 'react';
-import { CiHome } from 'react-icons/ci';
-import { ImStatsDots } from 'react-icons/im';
-import { IoTimeOutline } from 'react-icons/io5';
-import { Link, NavLink } from 'react-router';
+import React from "react";
+import { CiHome } from "react-icons/ci";
+import { ImStatsDots } from "react-icons/im";
+import { IoTimeOutline } from "react-icons/io5";
+import { Link, NavLink } from "react-router";
 
 const Navbar = () => {
-    return (
-        <div className='flex justify-between  border border-gray-300 p-5 rounded-2xl shadow-2xl'>
-            <a className="btn btn-ghost text-xl font-bold">Keen<span className="text-[#244D3F] font-bold">Keeper</span></a>
-            <ul className='flex gap-3  lg:gap-8 items-center'>
-                <li className='flex-1'><NavLink className={({ isActive }) =>
+  return (
+    <div className="flex justify-between  border border-gray-300 p-5 rounded-2xl shadow-2xl">
+      
+      <a className="btn btn-ghost text-xl font-bold">
+        Keen<span className="text-[#244D3F] font-bold">Keeper</span>
+      </a> 
+      <ul className="hidden md:flex gap-8 items-center">
+        <li className="flex-1">
+          <NavLink
+            className={({ isActive }) =>
               isActive
-                ?  'bg-[#244D3F] flex btn text-white font-bold'
+                ? "bg-[#244D3F] flex btn text-white font-bold"
                 : "flex btn font-bold"
-            } to="/"><CiHome />Home</NavLink></li>
-                <li><NavLink className={({ isActive }) =>
+            }
+            to="/"
+          >
+            <CiHome />
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) =>
               isActive
-                ?  'bg-[#244D3F] btn  text-white font-bold'
+                ? "bg-[#244D3F] btn  text-white font-bold"
                 : "flex btn font-bold"
-            } to="/timeline"><IoTimeOutline />Timeline</NavLink></li>
-                <li><NavLink className={({ isActive }) =>
+            }
+            to="/timeline"
+          >
+            <IoTimeOutline />
+            Timeline
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) =>
               isActive
-                ?  'bg-[#244D3F] btn text-white font-bold'
+                ? "bg-[#244D3F] btn text-white font-bold"
                 : "flex btn font-bold"
-            } to="/stats"><ImStatsDots />Stats</NavLink></li>
-            </ul>
-        </div>
-    );
+            }
+            to="/stats"
+          >
+            <ImStatsDots />
+            Stats
+          </NavLink>
+        </li>
+      </ul>
+      {/* daisy ui */}
+      <div className="dropdown dropdown-end md:hidden lg:hidden">
+  <div tabIndex={0} role="button" className="btn m-1">Menu  ⬇️</div>
+  <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+     <li className="flex-1">
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? "bg-[#244D3F] flex btn text-white font-bold"
+                : "flex btn font-bold"
+            }
+            to="/"
+          >
+            <CiHome />
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? "bg-[#244D3F] btn  text-white font-bold"
+                : "flex btn font-bold"
+            }
+            to="/timeline"
+          >
+            <IoTimeOutline />
+            Timeline
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? "bg-[#244D3F] btn text-white font-bold"
+                : "flex btn font-bold"
+            }
+            to="/stats"
+          >
+            <ImStatsDots />
+            Stats
+          </NavLink>
+        </li>
+  </ul>
+</div>
+    </div>
+  );
 };
 
 export default Navbar;
